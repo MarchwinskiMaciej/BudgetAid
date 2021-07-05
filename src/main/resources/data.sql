ca70439cf57a60480c3168852a086b9b2be66ebb
Maciej Marchwinski <maciej.marchwinski@misys.com>
2021-07-05 16:50:51 +0200

    Plug-in h2 ext database with initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into REGISTRY (id, balance, registry_name) values (1, 1000.0, 'Wallet');
+insert into REGISTRY (id, balance, registry_name) values (2, 5000.0, 'Savings');
+insert into REGISTRY (id, balance, registry_name) values (3, 0.0, 'Insurance policy');
+insert into REGISTRY (id, balance, registry_name) values (4, 0.0, 'Food expenses');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,1 @@
+create table REGISTRY(ID integer not null, REGISTRY_NAME varchar(255), BALANCE double, primary key (ID))
