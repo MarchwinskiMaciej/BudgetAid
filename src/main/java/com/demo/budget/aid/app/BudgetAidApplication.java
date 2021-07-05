b378dafff44bec7a94a9391be23943d81b38ba55
Maciej Marchwinski <maciej.marchwinski@misys.com>
2021-07-05 14:08:19 +0200

    Initialize spring app

---
+++ src/main/java/com/demo/budget/aid/app/BudgetAidApplication.java
@@ -0,0 +1,13 @@
+package com.demo.budget.aid.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetAidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetAidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/budget/aid/app/BudgetAidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.budget.aid.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetAidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
